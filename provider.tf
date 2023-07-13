a18fc66e122b62f1f0c7aa566f81efecb29d54db
SACHIN SINGH NEGI <55476118+sachinnegi001@users.noreply.github.com>
2023-07-13 14:36:01 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,6 +8,6 @@ terraform {
 }
 provider "newrelic" {
   account_id = "3930391"
-  api_key    = "NRAK-7Z0SHXV9L143Z1YMF0X2JM9DX8Z" # usually prefixed with 'NRAK'
+  api_key    = "skugbsvb-----------------" # usually prefixed with 'NRAK'
   region     = "US"                               # Valid regions are US and EU
 }
